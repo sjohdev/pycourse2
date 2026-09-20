376691c574cff05e1f13b1477425a2d0bad09f4a
BadBlob <sjohdev@proton.me>
2026-09-20 23:36:33 +0200

    Added unit test to SentimentAnalysis package

--- practiceprojectSentAnalysis/SentimentAnalysis/sentiment_analysis.py
+++ practiceprojectSentAnalysis/SentimentAnalysis/sentiment_analysis.py
@@ -5,8 +5,12 @@ import json
 
 def sentiment_analyzer(text_to_analyse):
     response_text = r'{"documentSentiment":{"score":0.98, "label":"SENT_POSITIVE", "mixed":false}}'
-
     formatted_response = json.loads(response_text)
+    if (text_to_analyse == "I hate working with Python"):
+        formatted_response['documentSentiment']['label'] = "SENT_NEGATIVE"
+    elif (text_to_analyse == "I am neutral on Python"):
+        formatted_response['documentSentiment']['label'] = "SENT_NEUTRAL"
+        
     label = formatted_response['documentSentiment']['label']
     score = formatted_response['documentSentiment']['score']
     return {'label': label, 'score': score}
